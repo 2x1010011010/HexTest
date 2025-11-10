ced12a98d4d3b5fa2293514b64718b3a20dc59af
Sergei Anisimov <s.anisimov.it@gmail.com>
2025-11-10 17:50:22 +0100

    Update ButtonBase.cs

--- Assets/HexaSortTest/CodeBase/GameLogic/UI/ButtonBase.cs
+++ Assets/HexaSortTest/CodeBase/GameLogic/UI/ButtonBase.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HexaSortTest.CodeBase.GameLogic.UI
 {
-  public class ButtonBase : MonoBehaviour
+  public abstract class ButtonBase : MonoBehaviour
   {
-    
+    [SerializeField] protected Button _button;
+
+    public Button Button => _button;
+
+    private void OnEnable()
+    {
+      _button.onClick.AddListener(ButtonClick);
+    }
+
+    private void OnDisable()
+    {
+      _button.onClick.RemoveListener(ButtonClick);
+    }
+
+    public void SetActive(bool active)
+    {
+      _button.enabled = active;
+    }
+
+    protected abstract void ButtonClick();
   }
 }
